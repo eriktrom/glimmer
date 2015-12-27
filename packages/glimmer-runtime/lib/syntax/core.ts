8baffda4994625660e005bc6b68adbd5e27c82be
Erik Trom <erik.trom@gmail.com>
2015-12-27 05:12:05 -0800

    remove debugger statements

--- packages/glimmer-runtime/lib/syntax/core.ts
+++ packages/glimmer-runtime/lib/syntax/core.ts
@@ -605,8 +605,6 @@ export class OpenElement extends StatementSyntax {
       let lookup = attributeInvocationToLookup(attrs, namedArgs);
       let template = compiler.templateFromTagContents();
       let templates = new Templates({ template, inverse: null });
-
-      debugger;
       compiler.append(new OpenComponentOpcode(component.compile(lookup, templates), namedArgs.compile(compiler)));
     } else {
       compiler.append(new OpenPrimitiveElementOpcode(this.tag));
