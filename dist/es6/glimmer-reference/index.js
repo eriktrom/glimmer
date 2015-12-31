c460bd463adf8793d3a4800ba07af86dea16e602
Erik Trom <erik.trom@gmail.com>
2015-12-31 00:03:16 -0800

    remove dist from git

--- dist/es6/glimmer-reference/index.js
+++
@@ -1,9 +0,0 @@
-export { ComputedReferenceBlueprint, InnerReferenceFactory, PropertyReference } from "./lib/references/descriptors";
-export { fork } from './lib/references/forked';
-export { CLASS_META, default as Meta, metaFor } from "./lib/meta";
-export { setProperty, notifyProperty } from './lib/object';
-export { PushPullReference } from './lib/references/push-pull';
-export * from './lib/types';
-export { default as UpdatableReference, referenceFromParts } from './lib/references/root';
-export { ConstReference } from './lib/references/const';
-export { ListManager, ListIterator, ListDelegate } from './lib/references/iterable';
